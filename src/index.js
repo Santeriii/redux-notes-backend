3495537d7fd9020e6e4ada82cf4cd71ed29aa659
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-12-25 16:33:42 +0200

    6 C -application done

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux' 
+import { composeWithDevTools } from 'redux-devtools-extension'
+import App from './App'
+import noteService from './services/notes'
+import noteReducer, { initializeNotes } from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer, composeWithDevTools())
+
+noteService.getAll().then(notes =>
+  store.dispatch(initializeNotes(notes))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
